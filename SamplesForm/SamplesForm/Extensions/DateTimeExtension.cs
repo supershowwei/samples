790e39d2db36741745fe84e3fae266d5114cfcbb
johnny <johnny@wantgoo.com>
2016-11-08 11:37:53 +0800

    add DateTimeExtension

---
+++ SamplesForm/SamplesForm/Extensions/DateTimeExtension.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SamplesForm.Extensions
+{
+    public static class DateTimeExtension
+    {
+        public static DateTime SpecifyTime(this DateTime me, int hour, int minute, int second)
+        {
+            return new DateTime(me.Year, me.Month, me.Day, hour, minute, second);
+        }
+    }
+}
