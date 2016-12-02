033bca223519b36d360f53fb52bc0308176c06c3
johnny <johnny@wantgoo.com>
2016-12-02 18:08:59 +0800

    add Singleton.cs

---
+++ SamplesForm/SamplesForm/Singleton.cs
@@ -0,0 +1,33 @@
+﻿using System.Runtime.Caching;
+
+namespace SamplesForm
+{
+    public class Singleton
+    {
+        public static readonly Singleton Instance = new Singleton();
+        private static readonly string Key = "MyKey";
+        private readonly ObjectCache objectCache = MemoryCache.Default;
+
+        private Singleton()
+        {
+        }
+
+        public object CacheObject
+        {
+            get
+            {
+                if (this.objectCache[Key] == null)
+                {
+                    this.LoadCacheObject();
+                }
+
+                return this.objectCache[Key];
+            }
+        }
+
+        private void LoadCacheObject()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
