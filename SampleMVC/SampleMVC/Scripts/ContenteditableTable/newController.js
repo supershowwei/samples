0f12a98116057b6be25351e882a4acb632c7b9e4
supershowwei <supershowwei@gmail.com>
2016-12-04 12:08:52 +0800

    complete $http handle HTTP and Service result fully

--- SampleMVC/SampleMVC/Scripts/ContenteditableTable/newController.js
+++ SampleMVC/SampleMVC/Scripts/ContenteditableTable/newController.js
@@ -10,10 +10,11 @@
             self.addReceipt = function () {
                 self.receipts.push(new Receipt());
             };
-
+            
+            // $http handle HTTP and Service result fully
             self.save = function () {
                 console.log(self.application);
-                $http.post("/application/new", self.application)
+                $http.post("/contenteditabletable/new", self.application)
                     .then(function ok(response) {
                             console.log(response);
                             if (response.data.isSuccess) {
@@ -28,8 +29,19 @@
                     .then(function success(data) {
                             console.log(data);
                         },
-                        function failure(data) {
+                        function unsuccess(data) {
+                            console.log(data);
+                            if (data.isFailure) {
+                                return $q.resolve(response.data);
+                            } else {
+                                return $q.reject(response.data);
+                            }
+                        })
+                    .then(function failure(data) {
                             console.error(data);
+                        },
+                        function warning(data) {
+                            console.warn(data);
                         });
             };
         }
