782b83c140f4c859b3f15b412d403e0114f12993
supershowwei <supershowwei@gmail.com>
2016-12-04 12:03:43 +0800

    add ContenteditableTable sample

---
+++ SampleMVC/SampleMVC/Scripts/ContenteditableTable/newApp.js
@@ -0,0 +1,1 @@
+﻿angular.module("newApp", ["ng-contenteditable"]);
---
+++ SampleMVC/SampleMVC/Scripts/ContenteditableTable/newController.js
@@ -0,0 +1,36 @@
+﻿angular.module("newApp")
+    .controller("newController",
+    [
+        "$http", "$q", "$window",
+        function ($http, $q, $window) {
+            var self = this;
+
+            self.receipts = [];
+
+            self.addReceipt = function () {
+                self.receipts.push(new Receipt());
+            };
+
+            self.save = function () {
+                console.log(self.application);
+                $http.post("/application/new", self.application)
+                    .then(function ok(response) {
+                            console.log(response);
+                            if (response.data.isSuccess) {
+                                return $q.resolve(response.data);
+                            } else {
+                                return $q.reject(response.data);
+                            }
+                        },
+                        function error(ex) {
+                            console.error(ex);
+                        })
+                    .then(function success(data) {
+                            console.log(data);
+                        },
+                        function failure(data) {
+                            console.error(data);
+                        });
+            };
+        }
+    ]);
