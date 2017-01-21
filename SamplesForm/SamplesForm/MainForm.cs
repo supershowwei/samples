eaf0cff327732255fbe9ac0027300bbb26c34b69
supershowwei <supershowwei@gmail.com>
2017-01-22 00:53:07 +0800

    add get up N levels directory functoin

--- SamplesForm/SamplesForm/MainForm.cs
+++ SamplesForm/SamplesForm/MainForm.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Reflection;
 using System.Windows.Forms;
 
 namespace SamplesForm
@@ -19,6 +21,28 @@ namespace SamplesForm
             var licenseKey = myMoneyZeroCracker.GenerateLicenseKey(email);
 
             var result = myMoneyZeroCracker.DecodeLicenseKey(licenseKey).Equals(email);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var currentPath = Assembly.GetExecutingAssembly().Location;
+
+            textBox1.AppendText("執行檔絕對路徑：" + currentPath + Environment.NewLine + Environment.NewLine);
+
+            var directory = File.GetAttributes(currentPath).HasFlag(FileAttributes.Directory)
+                ? currentPath
+                : Path.GetDirectoryName(currentPath);
+
+            textBox1.AppendText(
+                "UpLevel 1: " + Path.GetFullPath(Path.Combine(directory, @"..\")) + Environment.NewLine);
+            textBox1.AppendText(
+                "UpLevel 2: " + Path.GetFullPath(Path.Combine(directory, @"..\..\")) + Environment.NewLine);
+            textBox1.AppendText(
+                "UpLevel 3: " + Path.GetFullPath(Path.Combine(directory, @"..\..\..\")) + Environment.NewLine);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
         }
     }
 }
