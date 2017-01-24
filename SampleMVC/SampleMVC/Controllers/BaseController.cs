408481ba84847009b94a7be60c9066c65526903b
supershowwei <supershowwei@gmail.com>
2017-01-24 20:51:59 +0800

    add BaseController, JsonNetResult

---
+++ SampleMVC/SampleMVC/Controllers/BaseController.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+using System.Web.Mvc;
+
+namespace SampleMVC.Controllers
+{
+    public abstract class BaseController : Controller
+    {
+        protected override JsonResult Json(
+            object data,
+            string contentType,
+            Encoding contentEncoding,
+            JsonRequestBehavior behavior)
+        {
+            if (behavior.Equals(JsonRequestBehavior.DenyGet) && (this.Request != null)
+                && string.Equals(this.Request.HttpMethod, "GET"))
+            {
+                return new JsonResult();
+            }
+
+            return new JsonNetResult
+            {
+                Data = data,
+                ContentType = contentType,
+                ContentEncoding = contentEncoding,
+                JsonRequestBehavior = behavior
+            };
+        }
+    }
+}
---
+++ SampleMVC/SampleMVC/Controllers/JsonNetResult.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Web.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace SampleMVC.Controllers
+{
+    public class JsonNetResult : JsonResult
+    {
+        public JsonNetResult()
+        {
+            this.SerializerSettings = new JsonSerializerSettings
+                                          {
+                                              ContractResolver = new CamelCasePropertyNamesContractResolver()
+                                          };
+        }
+
+        public JsonSerializerSettings SerializerSettings { get; set; }
+
+        public Formatting Formatting { get; set; }
+
+        public override void ExecuteResult(ControllerContext context)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+
+            var response = context.HttpContext.Response;
+
+            response.ContentType = string.IsNullOrEmpty(this.ContentType) ? "application/json" : this.ContentType;
+
+            if (this.ContentEncoding != null)
+            {
+                response.ContentEncoding = this.ContentEncoding;
+            }
+
+            if (this.Data != null)
+            {
+                var writer = new JsonTextWriter(response.Output) { Formatting = this.Formatting };
+
+                JsonSerializer.Create(this.SerializerSettings).Serialize(writer, this.Data);
+
+                writer.Flush();
+            }
+        }
+    }
+}
