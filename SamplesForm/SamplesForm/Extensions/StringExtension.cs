acf8fb1c49f637f36dcfc7ef34232b0845f0163a
johnny <johnny@wantgoo.com>
2017-02-06 10:22:32 +0800

    add string.Right extension

--- SamplesForm/SamplesForm/Extensions/StringExtension.cs
+++ SamplesForm/SamplesForm/Extensions/StringExtension.cs
@@ -10,5 +10,12 @@ namespace SamplesForm.Extensions
 
             return me.Length > length ? me.Substring(0, length) : me;
         }
+
+        public static string Right(this string me, int length)
+        {
+            length = Math.Max(length, 0);
+
+            return me.Length > length ? me.Substring(me.Length - length, length) : me;
+        }
     }
 }
