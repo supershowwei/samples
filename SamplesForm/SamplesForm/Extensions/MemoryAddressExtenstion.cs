7c08b14f7d58a2cb176a00227f67abc174c52a02
supershowwei <supershowwei@gmail.com>
2017-03-23 19:31:03 +0800

    add MemoryAddressExtenstion

---
+++ SamplesForm/SamplesForm/Extensions/MemoryAddressExtenstion.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SamplesForm.Extensions
+{
+    public static class MemoryAddressExtenstion
+    {
+        public static string Address(this object me)
+        {
+            unsafe
+            {
+                TypedReference tr = __makeref(me);
+                var ptr = **(IntPtr**)&tr;
+                return "0x" + ptr.ToString("X");
+            }
+        }
+    }
+}
