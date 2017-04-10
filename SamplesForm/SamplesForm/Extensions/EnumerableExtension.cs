e7fcedc7965c96eb9e8f04a829a2b90c41e42143
johnny <johnny@wantgoo.com>
2017-04-10 11:36:19 +0800

    add EnumerableExtension.cs and TakeLast()

---
+++ SamplesForm/SamplesForm/Extensions/EnumerableExtension.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SamplesForm.Extensions
+{
+    public static class EnumerableExtension
+    {
+        public static IEnumerable<T> TakeLast<T>(this IEnumerable<T> me, int count)
+        {
+            return me.Skip(Math.Max(0, me.Count() - count));
+        }
+    }
+}
