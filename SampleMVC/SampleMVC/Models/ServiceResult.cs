4c5b29165b9ee77683f0e375db940f9515a480ea
johnny <johnny@wantgoo.com>
2017-04-14 12:39:37 +0800

    add ExceptionInterceptor

---
+++ SampleMVC/SampleMVC/Models/ServiceResult.cs
@@ -0,0 +1,11 @@
+﻿namespace SampleMVC.Models
+{
+    public abstract class ServiceResult
+    {
+        public bool IsSuccess { get; set; }
+
+        public bool IsFailure { get; set; }
+
+        public string Message { get; set; }
+    }
+}
