c2e4e0f0dbb513c514e70db507897551e23ab985
johnny <johnny@wantgoo.com>
2017-04-14 17:39:03 +0800

    update CacheKeyHelper

--- SampleMVC/SampleMVC/Helpers/CacheKeyHelper.cs
+++ SampleMVC/SampleMVC/Helpers/CacheKeyHelper.cs
@@ -9,22 +9,33 @@ namespace SampleMVC.Helpers
     {
         private static readonly Regex NameRegex = new Regex(@"{(?<name>\w*)}");
 
-        public static string GenerateKey(MethodInfo method, ParameterInfo[] parameters, object[] arguments, string input)
+        public static string GenerateKey(
+            MethodInfo method,
+            ParameterInfo[] parameters,
+            object[] arguments,
+            string input)
         {
-            var argumentsWithName = GenerateArgumentsWithName(parameters, arguments);
-
-            while (NameRegex.IsMatch(input))
+            if (string.IsNullOrEmpty(input))
             {
-                input = NameRegex.Replace(
-                    input,
-                    m =>
-                    {
-                        var name = m.Groups["name"].Value;
+                input = string.Join(",", arguments.Select(x => x?.ToString() ?? string.Empty));
+            }
+            else
+            {
+                var argumentsWithName = GenerateArgumentsWithName(parameters, arguments);
 
-                        return argumentsWithName.ContainsKey(name) && argumentsWithName[name] != null
-                                   ? argumentsWithName[name].ToString()
-                                   : string.Empty;
-                    });
+                while (NameRegex.IsMatch(input))
+                {
+                    input = NameRegex.Replace(
+                        input,
+                        m =>
+                            {
+                                var name = m.Groups["name"].Value;
+
+                                return argumentsWithName.ContainsKey(name) && argumentsWithName[name] != null
+                                           ? argumentsWithName[name].ToString()
+                                           : string.Empty;
+                            });
+                }
             }
 
             return $"{method.DeclaringType.Name}.{method.Name}({input})";
