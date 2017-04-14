6b18f72f49bf61a72f4d47e424f0c5caa133b349
johnny <johnny@wantgoo.com>
2017-04-14 13:06:13 +0800

    update CacheKeyAttribute

--- SampleMVC/SampleMVC/Attributes/CacheKeyAttribute.cs
+++ SampleMVC/SampleMVC/Attributes/CacheKeyAttribute.cs
@@ -5,14 +5,17 @@ namespace SampleMVC.Attributes
     [AttributeUsage(AttributeTargets.Method, Inherited = false)]
     public class CacheKeyAttribute : Attribute
     {
-        public CacheKeyAttribute(string template, int db = 0)
+        public CacheKeyAttribute(string template, int db = 0, TimeSpan? timeout = null)
         {
             this.Template = template;
             this.Db = db;
+            this.Timeout = timeout;
         }
 
         public int Db { get; set; }
 
         public string Template { get; set; }
+
+        public TimeSpan? Timeout { get; set; }
     }
 }
