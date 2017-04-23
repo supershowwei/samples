d674c1ddd7ba1c6207de90cca8cabac8d7efbb17
supershowwei <supershowwei@gmail.com>
2017-04-23 11:27:12 +0800

    add ControllerExtension.cs

---
+++ SampleMVC/SampleMVC/Controllers/ControllerExtension.cs
@@ -0,0 +1,18 @@
+﻿using System.Net;
+using System.Web.Mvc;
+
+namespace SampleMVC.Controllers
+{
+    public static class ControllerExtension
+    {
+        public static HttpStatusCodeResult HttpInternalServerError(this Controller me, string statusDescription = null)
+        {
+            return new HttpStatusCodeResult(HttpStatusCode.InternalServerError, statusDescription);
+        }
+
+        public static HttpStatusCodeResult HttpBadRequest(this Controller me, string statusDescription = null)
+        {
+            return new HttpStatusCodeResult(HttpStatusCode.BadRequest, statusDescription);
+        }
+    }
+}
