0a8a97743ebb2db7a324a118f92bc39e1a540560
johnny <johnny@wantgoo.com>
2017-05-22 09:40:11 +0800

    add GenerateUpdatingSql in DapperExtension

---
+++ SamplesForm/SamplesForm/Model/Parameters/Field.cs
@@ -0,0 +1,22 @@
+﻿namespace SamplesForm.Model.Parameters
+{
+    public class Field<T>
+    {
+        public Field(T value)
+        {
+            this.Value = value;
+        }
+
+        public T Value { get; private set; }
+
+        public static implicit operator Field<T>(T value)
+        {
+            return new Field<T>(value);
+        }
+
+        public static implicit operator T(Field<T> field)
+        {
+            return field.Value;
+        }
+    }
+}
