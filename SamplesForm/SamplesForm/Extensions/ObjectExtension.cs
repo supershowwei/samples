6b0fe41bfb2548476f0286f484739ca1dec62127
johnny <johnny@wantgoo.com>
2017-05-26 10:14:56 +0800

    add ObjectExtension.cs

---
+++ SamplesForm/SamplesForm/Extensions/ObjectExtension.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SamplesForm.Extensions
+{
+    public static class ObjectExtension
+    {
+        public static bool ExtEquals<T>(this T me, T other) where T : IEquatable<T>
+        {
+            if ((me == null) ^ (other == null)) return false;
+
+            return me == null || me.Equals(other);
+        }
+    }
+}
