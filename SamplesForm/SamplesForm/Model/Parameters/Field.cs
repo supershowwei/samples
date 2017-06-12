bb547c02001f2f6a55827be7bcdbdca89107dc35
johnny <johnny@wantgoo.com>
2017-06-12 14:15:10 +0800

    update Field.cs

--- SamplesForm/SamplesForm/Model/Parameters/Field.cs
+++ SamplesForm/SamplesForm/Model/Parameters/Field.cs
@@ -1,4 +1,6 @@
-﻿namespace SamplesForm.Model.Parameters
+﻿using System;
+
+namespace SamplesForm.Model.Parameters
 {
     public class Field<T>
     {
@@ -9,6 +11,11 @@
 
         public T Value { get; private set; }
 
+        public static implicit operator Field<T>(long value)
+        {
+            return CreateObjectOfT(value);
+        }
+
         public static implicit operator Field<T>(T value)
         {
             return new Field<T>(value);
@@ -17,6 +24,16 @@
         public static implicit operator T(Field<T> field)
         {
             return field.Value;
+        }
+
+        private static Field<T> CreateObjectOfT(object value)
+        {
+            if (value is T)
+            {
+                return new Field<T>((T)value);
+            }
+
+            return new Field<T>((T)Convert.ChangeType(value, typeof(T)));
         }
     }
 }
