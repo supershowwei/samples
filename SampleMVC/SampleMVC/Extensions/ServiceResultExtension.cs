ed03e6c15a5f3b6079f433931329d6232351a418
johnny <johnny@wantgoo.com>
2017-06-27 10:53:44 +0800

    add ServiceResultExtension.cs

---
+++ SampleMVC/SampleMVC/Extensions/ServiceResultExtension.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using SampleMVC.Models;
+
+namespace SampleMVC.Extensions
+{
+    public static class ServiceResultExtension
+    {
+        public static TResult To<TSource, TResult>(this TSource me, Func<TSource, TResult> selector)
+            where TSource : ServiceResult
+        {
+            return selector(me);
+        }
+    }
+}
