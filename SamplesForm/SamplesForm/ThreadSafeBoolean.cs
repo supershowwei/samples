0093e9efe593a56010287cd43b02ed8231cf1966
supershowwei <supershowwei@gmail.com>
2017-07-02 16:22:02 +0800

    add ThreadSafeBoolean.cs

---
+++ SamplesForm/SamplesForm/ThreadSafeBoolean.cs
@@ -0,0 +1,42 @@
+﻿using System.Threading;
+
+namespace SamplesForm
+{
+    public class ThreadSafeBoolean
+    {
+        private int interlockedInteger;
+
+        public ThreadSafeBoolean(bool value)
+        {
+            this.Value = value;
+        }
+
+        private bool Value
+        {
+            get
+            {
+                return this.interlockedInteger == 1;
+            }
+
+            set
+            {
+                Interlocked.Exchange(ref this.interlockedInteger, value ? 1 : 0);
+            }
+        }
+
+        public static implicit operator bool(ThreadSafeBoolean b)
+        {
+            return b.Value;
+        }
+
+        public static implicit operator ThreadSafeBoolean(bool b)
+        {
+            return new ThreadSafeBoolean(b);
+        }
+
+        public override string ToString()
+        {
+            return this.Value.ToString();
+        }
+    }
+}
