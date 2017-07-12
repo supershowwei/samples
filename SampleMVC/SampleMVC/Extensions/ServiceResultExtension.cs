23ff241583958a83227bd0667d044aafdc41d519
johnny <johnny@wantgoo.com>
2017-07-12 13:01:43 +0800

    rename To to Cast

--- SampleMVC/SampleMVC/Extensions/ServiceResultExtension.cs
+++ SampleMVC/SampleMVC/Extensions/ServiceResultExtension.cs
@@ -5,7 +5,7 @@ namespace SampleMVC.Extensions
 {
     public static class ServiceResultExtension
     {
-        public static TResult To<TSource, TResult>(this TSource me, Func<TSource, TResult> selector)
+        public static TResult Cast<TSource, TResult>(this TSource me, Func<TSource, TResult> selector)
             where TSource : ServiceResult
         {
             return selector(me);
