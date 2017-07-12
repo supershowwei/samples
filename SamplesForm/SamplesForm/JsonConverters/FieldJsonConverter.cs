e6868357b4312c36571e7b4ca03bee83036c8b80
johnny <johnny@wantgoo.com>
2017-07-12 12:59:56 +0800

    add FieldJsonConverter.cs

---
+++ SamplesForm/SamplesForm/JsonConverters/FieldJsonConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Newtonsoft.Json;
+using SamplesForm.Model.Parameters;
+
+namespace SamplesForm.JsonConverters
+{
+    public class FieldJsonConverter : JsonConverter
+    {
+        public override bool CanWrite => false;
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            return Activator.CreateInstance(objectType, reader.Value);
+        }
+
+        public override bool CanConvert(Type objectType)
+        {
+            return objectType.IsGenericType && objectType.GetGenericTypeDefinition() == typeof(Field<>);
+        }
+    }
+}
