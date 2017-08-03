c58e819f22e70fead8292bc278ad19b114c7e3e9
johnny <johnny@wantgoo.com>
2017-08-03 13:50:01 +0800

    add ExceptionHandlerAttribute.cs

---
+++ SampleMVC/SampleMVC/Filters/ExceptionHandlerAttribute.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Web.Mvc;
+
+namespace SampleMVC.Filters
+{
+    public class ExceptionHandlerAttribute : FilterAttribute, IExceptionFilter
+    {
+        public void OnException(ExceptionContext filterContext)
+        {
+            WriteLog(
+                "{0} '{1}' occur exception '{2}'\r\n\r\n Arguments:\r\n{3}\r\n\r\nStackTrace:\r\n{4}",
+                filterContext.HttpContext.Request.HttpMethod,
+                filterContext.HttpContext.Request.Url.AbsoluteUri,
+                filterContext.Exception.GetBaseException().Message,
+                TryGetString(filterContext.HttpContext.Request.InputStream),
+                filterContext.Exception.StackTrace);
+        }
+
+        private static string TryGetString(Stream inputStream)
+        {
+            try
+            {
+                if (inputStream.Length > 0)
+                {
+                    using (var sr = new StreamReader(inputStream, Encoding.UTF8))
+                    {
+                        sr.BaseStream.Seek(0, SeekOrigin.Begin);
+
+                        return sr.ReadToEnd();
+                    }
+                }
+            }
+            catch
+            {
+                // ignored
+            }
+
+            return string.Empty;
+        }
+
+        private static void WriteLog(string format, params object[] args)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
