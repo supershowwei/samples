e04b86ef91f9f199fd5f57af3f5260188817eda9
johnny <johnny@wantgoo.com>
2017-11-07 18:46:01 +0800

    update Get size of all tables in database.sql，改成用 MB 為單位。

--- SQL Statements/Get size of all tables in database.sql
+++ SQL Statements/Get size of all tables in database.sql
@@ -2,9 +2,9 @@ SELECT
     [t].[NAME] AS [TableName]
    ,[s].[Name] AS [SchemaName]
    ,[p].[rows] AS [RowCounts]
-   ,SUM([a].[total_pages]) * 8 AS [TotalSpaceKB]
-   ,SUM([a].[used_pages]) * 8 AS [UsedSpaceKB]
-   ,(SUM([a].[total_pages]) - SUM([a].[used_pages])) * 8 AS [UnusedSpaceKB]
+   ,(SUM([a].[total_pages]) * 8) / 1024 AS [TotalSpaceMB]
+   ,(SUM([a].[used_pages]) * 8) / 1024 AS [UsedSpaceMB]
+   ,((SUM([a].[total_pages]) - SUM([a].[used_pages])) * 8) / 1024 AS [UnusedSpaceMB]
 FROM [sys].[tables] [t]
 INNER JOIN [sys].[indexes] [i]
     ON [t].OBJECT_ID = [i].object_id
