9381707274aebc40515edea408f1fb348fb0d3c6
johnny <johnny@wantgoo.com>
2018-10-28 22:31:51 +0800

    udpate 抓增刪改資料的兇手.sql

--- SQL Statements/抓增刪改資料的兇手.sql
+++ SQL Statements/抓增刪改資料的兇手.sql
@@ -5,17 +5,17 @@ SELECT
    ,c.client_tcp_port
    ,c.local_net_address
    ,c.local_tcp_port
-   ,d.[name] AS local_database_name
+   ,d.[Name] AS local_database_name
    ,s.login_name
    ,(SELECT
             ib.event_info
         FROM sys.dm_exec_input_buffer(r.session_id, r.request_id) ib)
     AS sql_text
-FROM sys.dm_exec_connections c
-INNER JOIN sys.dm_exec_sessions s
-    ON c.session_id = s.session_id
+FROM sys.dm_exec_sessions s
+INNER JOIN sys.dm_exec_connections c
+    ON s.session_id = c.session_id
 INNER JOIN sys.dm_exec_requests r
-    ON c.session_id = r.session_id
+    ON s.session_id = r.session_id
 INNER JOIN sys.databases d
     ON r.database_id = d.database_id
-WHERE c.session_id = @@spid;
+WHERE s.session_id = @@spid;
