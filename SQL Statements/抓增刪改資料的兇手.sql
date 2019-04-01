658b56f79cfad35b4e20b52afade0f6b22c0089c
johnny <johnny@wantgoo.com>
2019-04-01 10:13:22 +0800

    update 抓增刪改資料的兇手.sql

--- SQL Statements/抓增刪改資料的兇手.sql
+++ SQL Statements/抓增刪改資料的兇手.sql
@@ -1,21 +1,30 @@
-SELECT
-    s.[host_name]
-   ,s.program_name
-   ,c.client_net_address
-   ,c.client_tcp_port
-   ,c.local_net_address
-   ,c.local_tcp_port
-   ,d.[Name] AS local_database_name
-   ,s.login_name
-   ,(SELECT
-            ib.event_info
-        FROM sys.dm_exec_input_buffer(r.session_id, r.request_id) ib)
-    AS sql_text
-FROM sys.dm_exec_sessions s
-INNER JOIN sys.dm_exec_connections c
-    ON s.session_id = c.session_id
-INNER JOIN sys.dm_exec_requests r
-    ON s.session_id = r.session_id
-INNER JOIN sys.databases d
-    ON r.database_id = d.database_id
-WHERE s.session_id = @@spid;
+INSERT INTO Suspect([host_name]
+                            ,program_name
+                            ,client_net_address
+                            ,client_tcp_port
+                            ,local_net_address
+                            ,local_tcp_port
+                            ,local_database_name
+                            ,login_name
+                            ,sql_text)
+    SELECT
+        s.[host_name]
+        ,s.program_name
+        ,c.client_net_address
+        ,c.client_tcp_port
+        ,c.local_net_address
+        ,c.local_tcp_port
+        ,d.[name] AS local_database_name
+        ,s.login_name
+        ,(SELECT
+                ib.event_info
+            FROM sys.dm_exec_input_buffer(r.session_id, r.request_id) ib)
+        AS sql_text
+    FROM sys.dm_exec_sessions s
+    INNER JOIN sys.dm_exec_connections c
+        ON s.session_id = c.session_id
+    INNER JOIN sys.dm_exec_requests r
+        ON s.session_id = r.session_id
+    INNER JOIN sys.databases d
+        ON r.database_id = d.database_id
+    WHERE s.session_id = @@spid;
