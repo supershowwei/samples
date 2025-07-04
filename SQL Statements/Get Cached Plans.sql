3dabbd2cb943d4dbec6d3b4c7439ebeac3878508
Johnny <johnny@nocoffee.nowork>
2025-07-04 14:21:39 +0800

    Update Get Cached Plans.sql

--- SQL Statements/Get Cached Plans.sql
+++ SQL Statements/Get Cached Plans.sql
@@ -1,14 +1,17 @@
-SELECT TOP 100
-    cp.usecounts 'User Counts'
+SELECT
+    decp.usecounts 'User Counts'
    ,size_in_bytes / 1024.0 'Size(KB)'
-   ,cp.cacheobjtype 'Cache Object'
-   ,cp.objtype 'Obj Type'
-   ,DB_NAME(st.dbid) 'DB'
-   ,st.text 'T-SQL'
-   ,cp.plan_handle
-   ,qp.query_plan 'Query Plan'
-FROM sys.dm_exec_cached_plans cp
-CROSS APPLY sys.dm_exec_sql_text(plan_handle) st
-CROSS APPLY sys.dm_exec_query_plan(plan_handle) qp
-WHERE st.dbid = DB_ID()
-ORDER BY 1 DESC;
+   ,decp.cacheobjtype 'Cache Object'
+   ,decp.objtype 'Obj Type'
+   ,DB_NAME(dest.[dbid]) 'DB'
+   ,dest.[text] 'T-SQL'
+   ,HASHBYTES('SHA2_256', dest.[text]) AS [T-SQL_Hash]
+   ,decp.plan_handle
+   ,deqp.query_plan 'Query Plan'
+   ,'DBCC FREEPROCCACHE (0x' + CONVERT(VARCHAR(512), decp.plan_handle, 2) + ')' AS [DBCC FREEPROCCACHE Command]
+FROM sys.dm_exec_cached_plans decp
+CROSS APPLY sys.dm_exec_query_plan(plan_handle) deqp
+CROSS APPLY sys.dm_exec_sql_text(plan_handle) dest
+--WHERE st.dbid = DB_ID()
+--WHERE dest.[text] LIKE ''
+
