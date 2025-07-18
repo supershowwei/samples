f3152bfb5e44f1e7c8ad2d332b2fb25e6dbc1618
Johnny <johnny@nocoffee.nowork>
2025-07-18 16:32:02 +0800

    update

---
+++ SQL Statements/Find Query Plans With Parameter Sniffing Risk.sql
@@ -0,0 +1,39 @@
+-- 一支執行計劃只有一個 plan_handle，但執行次數很多；max 與 min 差距 > 10 倍，就值得懷疑：
+DECLARE @DBName VARCHAR(MAX) = 'WantGoo'
+
+;
+WITH stats
+AS
+(SELECT
+        qs.plan_handle
+       ,qs.query_hash
+       ,qs.statement_start_offset
+       ,qs.statement_end_offset
+       ,qs.execution_count
+       ,qs.min_logical_reads
+       ,qs.max_logical_reads
+       ,qs.min_elapsed_time
+       ,qs.max_elapsed_time
+       ,qs.total_logical_reads / qs.execution_count AS avg_reads
+       ,qs.total_elapsed_time / qs.execution_count AS avg_ms
+       ,qs.last_execution_time
+    FROM sys.dm_exec_query_stats qs)
+SELECT TOP (50)
+    DB_NAME(st.dbid) AS db_name
+   ,OBJECT_NAME(st.objectid, st.dbid) AS obj_name
+   ,SUBSTRING(st.text,
+    (stats.statement_start_offset / 2) + 1,
+    ((CASE stats.statement_end_offset
+        WHEN -1 THEN DATALENGTH(st.text)
+        ELSE stats.statement_end_offset
+    END)
+    - stats.statement_start_offset) / 2 + 1) AS stmt
+   ,stats.*
+   ,qp.query_plan
+FROM stats
+CROSS APPLY sys.dm_exec_sql_text(stats.plan_handle) AS st
+CROSS APPLY sys.dm_exec_query_plan(stats.plan_handle) AS qp
+WHERE stats.execution_count >= 5                               -- 篩掉偶發
+    AND stats.max_logical_reads > stats.min_logical_reads * 10   -- 10 倍落差
+    AND DB_NAME(st.dbid) = @DBName
+ORDER BY stats.max_logical_reads DESC;
--- SQL Statements/Get Cached Plans.sql
+++ SQL Statements/Get Cached Plans.sql
@@ -1,17 +1,19 @@
 SELECT
-    decp.usecounts 'User Counts'
-   ,size_in_bytes / 1024.0 'Size(KB)'
-   ,decp.cacheobjtype 'Cache Object'
-   ,decp.objtype 'Obj Type'
-   ,DB_NAME(dest.[dbid]) 'DB'
-   ,dest.[text] 'T-SQL'
-   ,HASHBYTES('SHA2_256', dest.[text]) AS [T-SQL_Hash]
-   ,decp.plan_handle
-   ,deqp.query_plan 'Query Plan'
-   ,'DBCC FREEPROCCACHE (0x' + CONVERT(VARCHAR(512), decp.plan_handle, 2) + ')' AS [DBCC FREEPROCCACHE Command]
+    decp.usecounts AS [UserCounts]
+   ,size_in_bytes / 1024.0 AS [SizeKB]
+   ,decp.cacheobjtype AS [CacheObject]
+   ,decp.objtype AS [ObjectType]
+   ,DB_NAME(dest.[dbid]) AS [DBName]
+   ,dest.[text] AS [TSQL]
+   ,CONVERT(VARCHAR(512), HASHBYTES('SHA2_256', dest.[text]), 2) AS [TSQLHash]
+   ,decp.plan_handle AS [PlanHandle]
+   ,'DBCC FREEPROCCACHE (0x' + CONVERT(VARCHAR(512), decp.plan_handle, 2) + ') WITH NO_INFOMSGS' AS [DBCCFREEPROCCACHECommand]
+   ,deqp.query_plan AS [Plan]
 FROM sys.dm_exec_cached_plans decp
 CROSS APPLY sys.dm_exec_query_plan(plan_handle) deqp
 CROSS APPLY sys.dm_exec_sql_text(plan_handle) dest
---WHERE st.dbid = DB_ID()
+--WHERE dest.dbid = DB_ID('WantGoo')
+--WHERE dest.dbid = DB_ID('twStocks')
 --WHERE dest.[text] LIKE ''
+
 
